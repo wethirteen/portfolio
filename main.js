55b1de114d3ce29277470865e460e81b08cbec7c
wethirteen <abstyle772@gmail.com>
2025-04-04 19:23:12 +0900

    2차 구현

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 AOS.init()
+
+// 0. 헤더
+// 0-1. 모바일 메뉴창 (by 헤더)
+const $menuButton = document.querySelector('.header-nav-button')
+const $menuLinkButton = document.querySelectorAll('.modal-menu-link')
+const $modalMenu = document.querySelector('.modal-menu')
+const $backdrop = document.querySelector('.backdrop')
+
+const toggleMenu = () => {
+  $menuButton.classList.toggle('is-open')
+  $modalMenu.classList.toggle('is-open')
+  $backdrop.classList.toggle('is-open')
+}
+
+const removeMenu = () => {
+  $backdrop.classList.remove('is-open')
+  $modalMenu.classList.remove('is-open')
+  $menuButton.classList.remove('is-open')
+}
+
+$menuButton.addEventListener('click', toggleMenu)
+$backdrop.addEventListener('click', removeMenu)
+$menuLinkButton.forEach((button) => {
+  button.addEventListener('click', toggleMenu)
+})
